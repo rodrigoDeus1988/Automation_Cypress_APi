c82b2c23cb0362e6b1fe186c20c968829538146b
60003350 <rodrigo.deus@midway.com.br>
2021-02-11 18:00:33 -0300

    Alterações

--- cypress/integration/backend.spec.js
+++ cypress/integration/backend.spec.js
@@ -17,16 +17,34 @@ describe ('Should test at a funcional level', () => {
           url:'https://barrigarest.wcaquino.me/signin', 
           body:{
               email: "a@a",    
-              redirecionar: false, 
+              redirecionar: false,  
               senha: "a"  
           } 
 
-      }).its('body.token').should('not.be.empty') 
-    }) 
+      }).its('body.token').should('not.be.empty')
+      .then(token => {    
+        cy.request({
+              url:'https://barrigarest.wcaquino.me/contas', 
+              method: 'POST',
+              headers:{ Authorization: 'bearer ${token}' } ,     
+              body: {  
+                nome:'Conta para saldo' 
+               } 
+         }).as('response')
+
+      })  
+      cy.get('@response').then(res =>{
+          expect(res.status).to.be.equal(201)
+          expect(res.status).to.have.property('id')
+          expect(res.status).to.have.property('nome','Conta para saldo')
+      })
+    })   
+
 
     it ('Should upadate an account', () => {
 
         
     })
 
-})
+}) 
+
